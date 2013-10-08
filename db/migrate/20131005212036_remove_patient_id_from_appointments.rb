7ae9dbde2f4f1ee2175b66aae76ef0f22357ffcb
Rashad Abdul-Salaam <rashad@getskinclick.com>
2013-10-08 09:55:21 -0500

    Updated RemovePatientId Migration with Down Migration

--- db/migrate/20131005212036_remove_patient_id_from_appointments.rb
+++ db/migrate/20131005212036_remove_patient_id_from_appointments.rb
@@ -1,6 +1,10 @@
 class RemovePatientIdFromAppointments < ActiveRecord::Migration
-  def change
+  def up
   	remove_column :appointments, :patient_id, :integer
+  end
+
+  def down
+  	add_column :appointments, :patient_id, :integer
   end
 
   
